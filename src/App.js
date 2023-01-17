193f7e839c679be21a9a265bc6df88a16f49c249
yosassenav <mromerovanessa.soy@gmail.com>
2023-01-16 22:40:55 -0500

    adding last commits

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import "./App.css";
-import { useEffect } from "react";
+import User from "./components/User";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [characters, setCharacters] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
   useEffect(() => {
-    fetch("https://swapi.dev/api/people/?search=")
+    fetch("https://swapi.dev/api/people/?search=" + inputText)
       .then((response) => response.json())
-      .then((data) => console.log(data));
-  }, []);
+      .then((data) => {
+        if (inputText.length > 0) {
+          setCharacters(data.results);
+        }
+      });
+  }, [inputText]);
 
-  const showInputText = () => {
-    console.log("Heeeeey!!!!!");
+  const filterByName = (evt) => {
+    setInputText(evt.target.value);
   };
+
+  const clickOnUser = (character) => {
+    setSelectedUser(character);
+  };
+
+  //const toggleModal = () => {};
+
   return (
     <div className="App">
       <h1>Star Wars Examples</h1>
-      <input onChange={showInputText} />
+      <input onChange={filterByName} />
+      <article>{inputText}</article>
+      {characters
+        ? characters.map((character) => {
+            return (
+              <User
+                key={character.name}
+                character={character}
+                handleClick={clickOnUser}
+                selectedcharacter={selectedUser}
+              />
+            );
+          })
+        : null}
     </div>
   );
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,16 @@
-const User = ({user, handleClick, selectedUser}) => {
-    return(
-        <div className={`cursor-pointer rounded-full flex gap-4 items-center my-3 border-2 border-solid border-green-200 ${selectedUser?.phone === user.phone ? 'bg-green-200' : ''}`} onClick={()=>{handleClick(user)}}>
-            <img alt={user.name.first} className="rounded-full" src={user.picture.medium}/>
-            <span>{user.name.title}. {user.name.first} {user.name.last}</span>
-        </div>
-    )
-}
+const character = ({ character, handleClick, selectedcharacter }) => {
+  return (
+    <div
+      className={`cursor-pointer rounded-full flex gap-4 items-center my-3 border-2 border-solid border-green-200 `}
+      onClick={() => {
+        handleClick(character);
+      }}
+    >
+      <span>
+        {character.name}. {character.height} {character.mass}
+      </span>
+    </div>
+  );
+};
 
-export default User;
+export default character;
