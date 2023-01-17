c4c5338cb9d988eb1054e8ca01d2ee76928e03cf
yosassenav <mromerovanessa.soy@gmail.com>
2023-01-16 21:03:30 -0500

    adding changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("https://swapi.dev/api/people/?search=")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  const showInputText = () => {
+    console.log("Heeeeey!!!!!");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star Wars Examples</h1>
+      <input onChange={showInputText} />
     </div>
   );
 }
